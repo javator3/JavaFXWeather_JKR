5f948e72893dffbea7f1b1016e54abed3846e68b
Joanna Krzyżanowska <joanna.krzyzanowska@o2.pl>
2019-02-12 14:56:04 +0100

    "RootController uzupełniony - działa"

--- src/main/java/pl/sda/openweather/Main.java
+++ src/main/java/pl/sda/openweather/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -21,6 +22,7 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 600, 350));
         primaryStage.show();
     }
+
 
 
 }
